b37ec8b272f084981f6b502b53e089602a78187d
dhana <dhananjays@mail.usf.edu>
2020-03-13 12:40:05 -0400

    test

---
+++ WebApplication1/Data/WebApplication1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace WebApplication1.Data
+{
+    public class WebApplication1Context : DbContext
+    {
+        public WebApplication1Context (DbContextOptions<WebApplication1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContosoUniversity.Models.Student> Student { get; set; }
+    }
+}
